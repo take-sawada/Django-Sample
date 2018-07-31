2f7e4dad46a8f1aeea793c3ed82b089785ccddbf
Takehiro Sawada <takepierrot@gmail.com>
2018-07-31 22:52:41 +0900

    chapter4まで作成

--- my_app/books/urls.py
+++ my_app/books/urls.py
@@ -6,4 +6,8 @@ from . import views
 # ルーティングの設定
 urlpatterns = [
     url(r'^hello/$', views.hello, name='hello'),
+    # /post/以下に数字が渡された場合にマッチ
+    url(r'^post/(?P<post_id>[\d]+)/$', views.post, name='post'),
+    # /news/以下に文字列が渡された場合にマッチ
+    url(r'^news/(?P<slug>[-\w]+)/$', views.news, name='news'),
 ]
